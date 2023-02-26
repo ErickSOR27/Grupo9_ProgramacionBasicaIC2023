1a346f6339af3fe3e02cfcf8e63120590570ef3b
ErickSOR27 <erickOR27@hotmail.com>
2023-02-25 18:32:28 -0600

    Create Proyecto.py
    Update Proyecto.py

---
+++ Proyecto.py
@@ -0,0 +1,16 @@
+#Este va a ser nuestro archivo para la creación del proyecto
+while True:
+    opcion = int(input("Que desea realizar \n1- Registrar nuevo usuario \n2- Usuario Registrado\n3 -Configuracion Avanzada\n4- Salir\n "))
+    if opcion == 1:
+        print*(hola)
+    elif opcion ==2:
+        print(...)
+
+    elif opcion == 3 :
+        print("bye")
+
+    elif opcion == 4 :
+        print("Gracias por usar el programa")
+        break
+    else:print("Ingrese otro valor")
+
